93c426f67a278fe792d17f6f791d294bc6f1787f
liubinbin <lotosbin@gmail.com>
2014-09-02 11:37:08 +0800

    .

---
+++ datefromnow-dotnet-test/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using datefromnow_dotnet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace datefromnow_dotnet_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var now = DateTime.Now;
+            var timeSpan = now - now.AddSeconds(-10);
+            var s = timeSpan.ToDateStringFromNow();
+            Assert.AreEqual("10秒前", s);
+        }
+    }
+}
